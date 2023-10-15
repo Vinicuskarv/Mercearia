9cd23b11946d958b12559c64083a1548e1994a67
Vinicius <karvvinicius@gmail.com>
2023-10-15 22:43:51 +0100

    add

--- HOME.php
+++ HOME.php
@@ -32,18 +32,24 @@ $result = mysqli_query($conexao, $query);
                 endif;
                 unset($_SESSION['modo_adm']);
             ?>
-            <button type="submit" class="ButtonCartStory"><img src="./Icons/icons8-shopping-cart-30.png" alt="produtos"><label>2</label></button>
-
+            <button type="submit" class="ButtonCartStory" onClick="AbrirDivInfo()"><img src="./Icons/icons8-shopping-cart-30.png" alt="produtos"><label id="CartNumber"></label></button>
         </ul>
     </nav>
     <div class="container-fluid CampoTop">
         <div>
             <h1>ORGANIC</h1>
             <p>A comida orgânica é uma opção saudável e sustentável, cultivada sem pesticidas sintéticos e respeitando o meio ambiente.</p>
-            <input class="ButtonMaster" type="button" value="Sobre">
         </div>
-        
     </div>
+    <div class="CartCamp">
+        <div id="CartProdutos"></div><br>
+        <button class="ButtonCloseCart" onClick="FecharDivInfo()">X</button>
+        <form class="campEcomendas" id="campEcomendas" action="adicionar_ecomenda.php" method="post">
+            <input type="hidden" name="meuArrayInput" id="meuArrayInput" value="">
+            <button type="submit" class="buttoAdicionareEcomenda">Confirmar</button>
+            <button type="button" class="buttoAdicionareEcomenda" onClick="EscluirCart()">Excluir Carrinho</button>
+        </form>
+        </div>
     <header class="container CampoCards">
         <section class="row" id="produtos">
             <?php
@@ -66,9 +72,29 @@ $result = mysqli_query($conexao, $query);
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $row['Nome']; ?></h5>
                         <p>€ <?php echo number_format($row['Preco'], 2, '.', ','); ?></p>
-                        <div class="CardHover">
-                            <input type="number" name="quantidade" id="quantidade">
-                            <button type="submit" class="ButtonCart"><img src="./Icons/icons8-shopping-cart-30.png"  alt="Img produto"></button>
+                        <label class="CardHoverlabel">Stock: <?php echo $row['Quantidade']; ?></label>
+
+                        <div class="CardHover" >
+                            <?php
+                                if ($row['Quantidade'] > 0){
+                            ?>
+                            <div>
+                            <input type="number" name="quantidade" id="quantidade-<?php echo $row['id'] ?>" >
+                            
+                            <input type="hidden" name="Preco" value="<?php echo number_format($row['Preco'], 2, '.', ','); ?>" id="valor-<?php echo $row['id'] ?>">
+                            <input type="hidden" name="Nome" value="<?php echo $row['Nome']; ?>" id="nome-<?php echo $row['id'] ?>">
+                            <input type="hidden" name="Quantidade" value="<?php echo $row['Quantidade']; ?>" id="quantidade-atual-<?php echo $row['id'] ?>">
+
+                            <button type="submit" class="ButtonCart addToCartButton" data-product-id="<?php echo $row['id'] ?>"><img src="./Icons/icons8-shopping-cart-30.png"  alt="Img produto"></button>
+                            </div>
+                            <?php
+                                } else {
+                            ?>
+                            <label>Não há stock</label>
+                            <?php
+                                }
+                            ?>
+                            <div class ="CardHovInf" id="CardHovInf-<?php echo $row['id'] ?>"></div>
                         </div>      
                     </div>
                 </div>
@@ -80,9 +106,125 @@ $result = mysqli_query($conexao, $query);
         }
         ?>
         </section>
+        
     </header>
 
+    <script>
+        var carrinho = [];
+        var PrecoTotal = 0;
 
+        document.getElementById('meuArrayInput').value = JSON.stringify(carrinho);
+
+        function EscluirCart() {
+            carrinho = [];
+            var exibicao = document.getElementById('CartProdutos');
+            while (exibicao.firstChild) {
+                exibicao.removeChild(exibicao.firstChild);
+            }
+            atualizarQuantidade();
+        }
+
+        function AbrirDivInfo() {
+            var CartCampo = document.querySelectorAll('.CartCamp');
+            
+            CartCampo.forEach(function(element) {
+                element.classList.add('CardCampOn');
+            });
+        }
+        function FecharDivInfo() {
+            var CartCampo = document.querySelectorAll('.CartCamp');
+            
+            CartCampo.forEach(function(element) {
+                element.classList.remove('CardCampOn');
+            });
+        }
+
+        function atualizarExibicao(array) {
+            var exibicao = document.getElementById('CartProdutos');
+            exibicao.innerHTML = '';
+
+            array.forEach(function (item) {
+                
+                var linha = document.createElement('p');
+                linha.textContent = item[3] + ', Quantidade: ' + item[1] + ', Preço: ' + item[2];
+                exibicao.appendChild(linha);
+
+                var precoUnido = item[2] * item[1];
+                PrecoTotal += precoUnido;
+            });
+
+            var precoTotalFormatado = PrecoTotal.toFixed(2);
+            var totalLinha = document.createElement('p');
+            totalLinha.textContent = 'Preço Total: ' + precoTotalFormatado;
+            exibicao.appendChild(totalLinha);
+        }
+
+
+
+        function atualizarQuantidade() {
+            var quantidadeItens = document.getElementById('CartNumber');
+            quantidadeItens.textContent = carrinho.length;
+        }
+
+        atualizarQuantidade();
+
+        var addToCartButtons = document.querySelectorAll('.addToCartButton');
+
+        addToCartButtons.forEach(function(button) {
+            button.addEventListener('click', function() {
+                var produtoId = this.getAttribute('data-product-id');
+                var quantidadeInput = document.querySelector('#quantidade-'+produtoId);
+                var valorInput = document.querySelector('#valor-'+produtoId);
+                var NomeInput = document.querySelector('#nome-'+produtoId);
+                var quantidadeAtual = document.querySelector('#quantidade-atual-'+produtoId);
+                var exibi= document.getElementById('CardHovInf-'+produtoId);
+
+
+                if (quantidadeInput) {
+                    var quantidade = parseInt(quantidadeInput.value);
+                    var valor = valorInput.value;
+                    var nome = NomeInput.value;
+                    var QAtual = quantidadeAtual.value;
+                    console.log(QAtual);
+
+                    if (!isNaN(quantidade) && quantidade > 0) {
+                        if (quantidade <= QAtual){
+                            carrinho.push([produtoId, quantidade, valor, nome]);
+                            atualizarQuantidade();
+                            atualizarExibicao(carrinho);
+                            document.getElementById('meuArrayInput').value = JSON.stringify(carrinho);
+
+                            quantidadeInput.value = "";
+                            valorInput.value = "";
+                            while (exibi.firstChild) {
+                                exibi.removeChild(exibi.firstChild);
+                            }
+                        }else{
+                            while (exibi.firstChild) {
+                                exibi.removeChild(exibi.firstChild);
+                            }
+                            var aviso = document.createElement('p');
+                            aviso.textContent = "Stock atual: " + QAtual;
+                            aviso.style.color = 'black'; 
+
+                            exibi.appendChild(aviso);
+                        }
+                    } else {
+                        while (exibi.firstChild) {
+                                exibi.removeChild(exibi.firstChild);
+                            }
+                        var aviso = document.createElement('p');
+                            aviso.textContent = "Valor Invalido";
+                            aviso.style.color = 'black'; 
+                            exibi.appendChild(aviso);
+
+                    }
+                } else {
+                    console.log("Campo de entrada não encontrado para o produto com ID " + produtoId);
+                }
+            });
+        });
+    </script>   
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
---
+++ adicionar_ecomenda.php
@@ -0,0 +1,6 @@
+<?php 
+$meuArrayJSON = $_POST['meuArrayInput'];
+$meuArray = json_decode($meuArrayJSON);
+
+echo $meuArrayJSON;
+?>
